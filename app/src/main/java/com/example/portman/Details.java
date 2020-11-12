1498d7e1625b784548af531739a3076337b6b29a
himanshu kriplani <kriplani@usc.edu>
2020-11-12 00:27:45 -0800

    Save to SP Watchlist from details

--- app/src/main/java/com/example/portman/Details.java
+++ app/src/main/java/com/example/portman/Details.java
@@ -6,11 +6,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 
 import org.json.JSONObject;
 
@@ -20,6 +26,8 @@ import java.util.Map;
 public class Details extends AppCompatActivity {
     private static final String TAG = "Details";
     private TextView titleSymbolView;
+    HashMap<String,String> watchlist;
+    ImageButton starButton;
     String titleSymbol;
     HashMap<String,Integer> portfolio_qty= new HashMap<String,Integer>();
 //    HashMap<Integer, String> hash_map = new HashMap<Integer, String>();
@@ -38,39 +46,47 @@ public class Details extends AppCompatActivity {
 
         titleSymbol=currIntent.getStringExtra("symbol");
         Log.d(TAG, "onCreate: "+titleSymbol);
-        titleSymbolView.setText(titleSymbol);
+        watchlist=readFromSP("watchlist");
+        String company_name=watchlist.getOrDefault(titleSymbol,"Default");
+        titleSymbolView.setText(titleSymbol+company_name);
+
+
+        starButton =(ImageButton) findViewById(R.id.starToggle);
+        starButton.setOnClickListener(new View.OnClickListener()   {
+            public void onClick(View v)  {
+                try {
+                    //Go to Details;
+                    watchlist.put(titleSymbol,"Apple Inc.");
+                    insertToSP(watchlist,"watchlist");
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
     }
-    public void saveData() {
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+
+    private void insertToSP(HashMap<String, String> jsonMap,String data_) {
+        String jsonString = new Gson().toJson(jsonMap);
+        SharedPreferences sharedPreferences = getSharedPreferences("HashMap", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString(TEXT, "testDataa");
-//        editor.putBoolean(SWITCH1, switch1.isChecked());
+        editor.putString(data_, jsonString);
         editor.apply();
-        Toast.makeText(this, "Data saved", Toast.LENGTH_SHORT).show();
     }
 
-    private void saveMap(Map<String,Integer> inputMap){
-//        Gson gson=new Gson();
 
-//        SharedPreferences pSharedPref = getApplicationContext().getSharedPreferences("MyVariables", Context.MODE_PRIVATE);
-
-//        if (pSharedPref != null){
-//            JSONObject jsonObject = new JSONObject(inputMap);
-//            String jsonString = jsonObject.toString();
-//            SharedPreferences.Editor editor = pSharedPref.edit();
-//            editor.remove("My_map").commit();
-//            editor.putString("My_map", jsonString);
-//            editor.commit();
-//        }
-
-        SharedPreferences keyValues = getApplicationContext().getSharedPreferences("Your_Shared_Prefs", Context.MODE_PRIVATE);
-        SharedPreferences.Editor keyValuesEditor = keyValues.edit();
-
-        for (String s : portfolio_qty.keySet()) {
-            keyValuesEditor.putString(s, String.valueOf(portfolio_qty.get(s)));
-        }
-
-        keyValuesEditor.commit();
+    private HashMap<String, String> readFromSP(String data_){
+        SharedPreferences sharedPreferences = getSharedPreferences("HashMap", MODE_PRIVATE);
+        String defValue = new Gson().toJson(new HashMap<String, String>());
+        String json=sharedPreferences.getString(data_,defValue);
+        TypeToken<HashMap<String,String>> token = new TypeToken<HashMap<String,String>>() {};
+        HashMap<String,String> retrievedMap=new Gson().fromJson(json,token.getType());
+        return retrievedMap;
     }
+
+
+
+
 
 }
